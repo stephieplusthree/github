23a36967ab372c9e1b29c6477baeb42560d5b54d
stephanie <stephieplusthree@gmail.com>
2021-06-03 18:50:20 -0700

    moveAllVowels

--- codewars/7kyu/moveAllVowels.js
+++ codewars/7kyu/moveAllVowels.js
@@ -39,3 +39,15 @@ function moveVowel(input) {
      return nonVowls + vowels
       
 }
+
+//g tells to find all matches, not just the first.
+
+//i tells to be case insensitive.
+
+//What goes inside the // is the pattern.
+
+//[] tells to match any character in a set.
+
+//aeiou are the characters in the set.
+
+//[^aeiou] -> not a, e, i, or u
